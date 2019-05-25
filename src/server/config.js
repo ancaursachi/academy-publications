a0eed11b77b47f14b8f03dc1b4da294892d31a16
Anca Ioana Ursachi <u.ancaioana@gmail.com>
2019-05-25 16:30:41 +0300

    added s3 upload functionality of a manuscript on backend

--- src/server/config.js
+++ src/server/config.js
@@ -2,8 +2,12 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
-  jwtSecret: 'hipopotam',
-  jwtExpiresIn: '90000000000000000000',
-  port: '1000',
-  dbLink: 'mongodb://admin:admin16@ds243041.mlab.com:43041/academy-db',
+    jwtSecret: 'hipopotam',
+    jwtExpiresIn: '90000000000000000000',
+    port: '1000',
+    dbLink: 'mongodb://admin:admin16@ds243041.mlab.com:43041/academy-db',
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretKeyId: process.env.SECRET_KEY_ID,
+    manuscriptBucketName: process.env.MANUSCRIPTS_BUCKET_NAME
 }
